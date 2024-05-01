b81927c2130992bc62c10c52008ae59daf6eb495
notaika <ercamanalo@gmail.com>
2024-04-30 17:31:31 -0700

    added fallback font. finished rough responsive design of main video player for all breakpoints

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -4,9 +4,11 @@ export function VideoPlayer(props) {
     const mainVideo = props.src[0];
 
     return (
-        <video className="main__video" poster={mainVideo.image} alt="The current video playing" controls>
-            <source src={mainVideo.video} type="video/mp4" />
-        </video>
+        <div className="main__container">
+            <video className="main__video" poster={mainVideo.image} alt="The current video playing" controls>
+                <source src={mainVideo.video} type="video/mp4" />
+            </video>
+        </div>
     )
 }
 
