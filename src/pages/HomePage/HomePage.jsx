945c62f2ed521782f961f4646464924d03b77b0d
notaika <ercamanalo@gmail.com>
2024-05-08 16:09:29 -0700

    refactored useEffect functions for better readability

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,48 +9,46 @@ export default function HomePage() {
   const videoItem = useParams();
   const API_KEY = "18e3464d-cb9e-401c-9f58-8a48db055f71";
 
-  useEffect(() => {
-    async function fetchVideos() {
-      try {
+  async function fetchVideos() {
+    try {
+      const response = await axios.get(
+        `https://unit-3-project-api-0a5620414506.herokuapp.com/videos?api_key=${API_KEY}`
+      );
+      setVideoData(response.data);
+
+      console.log(`SUCCESS: Videos fetched.`);
+    } catch (error) {
+      console.error(`ERROR: Could not fetch videos, ${error}`);
+    }
+  }
+
+  useEffect(() => {fetchVideos()}, []);
+
+  async function fetchVideoItem() {
+    try {
+      if (!videoItem.id) {
         const response = await axios.get(
           `https://unit-3-project-api-0a5620414506.herokuapp.com/videos?api_key=${API_KEY}`
         );
-        setVideoData(response.data);
 
-        console.log(`SUCCESS: Videos fetched.`);
-      } catch (error) {
-        console.error(`ERROR: Could not fetch videos, ${error}`);
+        const featuredResponse = await axios.get(
+          `https://unit-3-project-api-0a5620414506.herokuapp.com/videos/${response.data[0].id}?api_key=${API_KEY}`
+        );
+        setFeaturedVideo(featuredResponse.data);
+
+        console.log(`SUCCESS: Featured video was set.`);
+      } else {
+        const featuredResponse = await axios.get(
+          `https://unit-3-project-api-0a5620414506.herokuapp.com/videos/${videoItem.id}?api_key=${API_KEY}`
+        );
+        setFeaturedVideo(featuredResponse.data);
       }
+    } catch (error) {
+      console.error(`ERROR: Unable to set featured video, ${error}`);
     }
-    fetchVideos();
-  }, []);
+  }
 
-  useEffect(() => {
-    async function fetchVideoItem() {
-      try {
-        if (!videoItem.id) {
-          const response = await axios.get(
-            `https://unit-3-project-api-0a5620414506.herokuapp.com/videos?api_key=${API_KEY}`
-          );
-
-          const featuredResponse = await axios.get(
-            `https://unit-3-project-api-0a5620414506.herokuapp.com/videos/${response.data[0].id}?api_key=${API_KEY}`
-          );
-          setFeaturedVideo(featuredResponse.data);
-
-          console.log(`SUCCESS: Featured video was set.`);
-        } else {
-          const featuredResponse = await axios.get(
-            `https://unit-3-project-api-0a5620414506.herokuapp.com/videos/${videoItem.id}?api_key=${API_KEY}`
-          );
-          setFeaturedVideo(featuredResponse.data);
-        }
-      } catch (error) {
-        console.error(`ERROR: Unable to set featured video, ${error}`);
-      }
-    }
-    fetchVideoItem();
-  }, [videoItem.id]);
+  useEffect(() => {fetchVideoItem()}, [videoItem.id]);
   return (
     <>
       <Main videoData={videoData} featuredVideo={featuredVideo} />
