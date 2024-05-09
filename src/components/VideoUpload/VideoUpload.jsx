50f4df25d82fec634868c24a65618ab3651cd989
notaika <ercamanalo@gmail.com>
2024-05-09 15:54:08 -0700

    finished responsive design for upload-page. added alert to know form was submitted (video uploaded).

--- src/components/VideoUpload/VideoUpload.jsx
+++ src/components/VideoUpload/VideoUpload.jsx
@@ -21,16 +21,17 @@ export default function VideoUpload() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     redirect('/')
     console.log('Form Submitted.')
+    alert('Video uploaded.')
   }
-
-  console.log(values.videoDescription, values.videoTitle)
 
   return (
     <section className="uploads">
       <div className="uploads__container">
         <h1 className="uploads__title">Upload Video</h1>
+        <form className="uploads__form" onSubmit={handleSubmit}>
         <div className="uploads__thumbnail">
           <h3 className="uploads__thumbnail-title uploads__header">
             Video Thumbnail
@@ -42,27 +43,30 @@ export default function VideoUpload() {
           />
         </div>
 
-        <form className="uploads__form" onSubmit={handleSubmit}>
-          <h3 className="uploads__header">Title Your Video</h3>
-          <input
-            type="text"
-            className="uploads__form-title"
-            name="videoTitle"
-            placeholder="Add a title to your video"
-            onChange={handleInputChange}
-            value={values.videoTitle}
-          />
-
-          <h3 className="uploads__header">Add a video description</h3>
-          <textarea
-            name="videoDescription"
-            className="uploads__form-description"
-            placeholder="Add a description to your video"
-            onChange={handleInputChange}
-            value={values.videoDescription}
-          ></textarea>
-          <button className="uploads__form-submit">PUBLISH</button>
+          <div className="uploads__form-inputs">
+            <h3 className="uploads__header">Title Your Video</h3>
+            <input
+              type="text"
+              className="uploads__form-title"
+              name="videoTitle"
+              placeholder="Add a title to your video"
+              onChange={handleInputChange}
+              value={values.videoTitle}
+            />
+            <h3 className="uploads__header">Add a video description</h3>
+            <textarea
+              name="videoDescription"
+              className="uploads__form-description"
+              placeholder="Add a description to your video"
+              onChange={handleInputChange}
+              value={values.videoDescription}
+            ></textarea>
+          </div>
         </form>
+        <div className="uploads__form-buttons">
+            <button onClick={handleSubmit} className="uploads__form-submit">PUBLISH</button>
+            <a href="/" className="uploads__form-cancel">CANCEL</a>
+          </div>
       </div>
     </section>
   );
